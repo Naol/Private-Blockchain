34169cb99bc0e24da0c9bd38a4670d439ec4404d
Naol <naolkebede95@gmail.com>
2021-03-09 14:16:37 +0300

    First Commit - Complete project

---
+++ BlockchainController.js
@@ -0,0 +1,153 @@
+/**
+ *          BlockchainController
+ *       (Do not change this code)
+ * 
+ * This class expose the endpoints that the client applications will use to interact with the 
+ * Blockchain dataset
+ */
+class BlockchainController {
+
+    //The constructor receive the instance of the express.js app and the Blockchain class
+    constructor(app, blockchainObj) {
+        this.app = app;
+        this.blockchain = blockchainObj;
+        // All the endpoints methods needs to be called in the constructor to initialize the route.
+        this.getBlockByHeight(); 
+        this.requestOwnership(); 
+        this.submitStar(); 
+        this.getBlockByHash(); // Not done
+        this.getStarsByOwner(); 
+        this.listall(); // For testing puroses
+        this.validateBChain();
+    }
+
+    // Enpoint to Get a Block by Height (GET Endpoint)
+    getBlockByHeight() {
+        this.app.get("/block/height/:height", async (req, res) => {
+            if(req.params.height) {
+                const height = parseInt(req.params.height);
+                let block = await this.blockchain.getBlockByHeight(height);
+                if(block){
+                    return res.status(200).json(block);
+                } else {
+                    return res.status(404).send("Block Not Found!");
+                }
+            } else {
+                return res.status(404).send("Block Not Found! Review the Parameters!");
+            }
+            
+        });
+    }
+    
+    //For testing purposes
+    listall() {
+        this.app.get("/listAll", async (req, res) => {
+            let blocks = await this.blockchain.listAll();
+            if(blocks){
+                let s = "";
+                blocks.forEach(element => {
+                    s += element.hash +", "
+                });
+                return res.status(200).json(s);
+            } else {
+                return res.status(404).send("Block Not Found!");
+            }
+        });
+    }
+
+    // Endpoint that allows user to request Ownership of a Wallet address (POST Endpoint)
+    requestOwnership() {
+        this.app.post("/requestValidation", async (req, res) => {
+            if(req.body.address) {
+                const address = req.body.address;
+                const message = await this.blockchain.requestMessageOwnershipVerification(address);
+                if(message){
+                    return res.status(200).json(message);
+                } else {
+                    return res.status(500).send("An error happened!");
+                }
+            } else {
+                return res.status(500).send("Check the Body Parameter!");
+            }
+        });
+    }
+
+    // Endpoint that allow Submit a Star, yu need first to `requestOwnership` to have the message (POST endpoint)
+    submitStar() {
+        this.app.post("/submitstar", async (req, res) => {
+            if(req.body.address && req.body.message && req.body.signature && req.body.star) {
+                const address = req.body.address;
+                const message = req.body.message;
+                const signature = req.body.signature;
+                const star = req.body.star;
+                try {
+                    let block = await this.blockchain.submitStar(address, message, signature, star);
+                    if(block){
+                        return res.status(200).json(block);
+                    } else {
+                        return res.status(500).send(block.error);
+                    }
+                } catch (error) {
+                    return res.status(500).send(block.error);
+                }
+            } else {
+                return res.status(500).send("Check the Body Parameter!");
+            }
+        });
+    }
+
+    // Validate the chain
+    validateBChain() {
+        this.app.get("/validatechain", async (req, res) => {
+            let error_list = await this.blockchain.validateChain();
+            if(error_list.length === 0){
+                return res.status(404).json("All Blocks are Valid. No Errors Found");
+            }else{
+                return res.status(200).json(error_list);
+            }
+        });
+    }
+
+    // This endpoint allows you to retrieve the block by hash (GET endpoint)
+    getBlockByHash() {
+        this.app.get("/block/hash/:hash", async (req, res) => {
+            if(req.params.hash) {
+                const hash = req.params.hash;
+                let blocks = await this.blockchain.getBlockByHash(hash);
+                if(blocks){
+                    return res.status(200).json(blocks);
+                } else {
+                    return res.status(404).send("No Blocks Found!");
+                }
+            } else {
+                return res.status(404).send("Blocks Not Found! Review the Parameters!");
+            }
+            
+        });
+    }
+
+    // This endpoint allows you to request the list of Stars registered by an owner
+    getStarsByOwner() {
+        this.app.get("/blocks/:address", async (req, res) => {
+            if(req.params.address) {
+                const address = req.params.address;
+                try {
+                    let stars = await this.blockchain.getStarsByWalletAddress(address);
+                    if(stars){
+                        return res.status(200).json(stars);
+                    } else {
+                        return res.status(404).send("Block Not Found!");
+                    }
+                } catch (error) {
+                    return res.status(500).send("An error happened!");
+                }
+            } else {
+                return res.status(500).send("Block Not Found! Review the Parameters!");
+            }
+            
+        });
+    }
+
+}
+
+module.exports = (app, blockchainObj) => { return new BlockchainController(app, blockchainObj);}
